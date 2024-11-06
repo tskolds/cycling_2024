bb4c7313b9737e21fe15cdd12343bb3a6fb0d151
Timothy Skolds <tskolds@colostate.edu>
2024-11-06 12:29:57 -0700

    made a nice grid

--- worldtour_2024.R
+++ worldtour_2024.R
@@ -20,11 +20,10 @@ wins_per_team <- ggplot(worldtour_2024,
   theme_bw() +
   labs(x = "Team",
        y = "Number of Wins",
-       title = "WT Wins by Team",
-       caption = "Data sourced from FirstCycling") +
+       title = "WT Wins by Team") +
   theme(axis.text.x = element_text(angle = 90)) +
+  theme(legend.position = "none") +
   scale_y_continuous(expand = expansion(mult = c(0, 0.05))) # Removes space below zero
-print(wins_per_team)
 
 # PT WT wins
 PT_wins <- worldtour_2024 %>%
@@ -37,10 +36,35 @@ wins_per_PT <- ggplot(PT_wins,
   theme_bw() +
   labs(x = "Team",
        y = "Number of Wins",
-       title = "WT Wins per ProTeam",
-       caption = "Data sourced from FirstCycling") +
+       title = "WT Wins per ProTeam") +
+  theme(legend.position = "none")
   scale_y_continuous(expand = expansion(mult = c(0, 0.05)))
-print(wins_per_PT)
+
+# Boxplot
+wins_team <- worldtour_2024 %>%
+  group_by(Team) %>%
+  summarise(Wins = n())
+
+wins_boxplot <- ggplot(wins_team,
+                       aes(y = Wins)) +
+  geom_boxplot(fill = "limegreen") +
+  theme_bw()
+
+wins_PT <- PT_wins %>%
+  group_by(Team) %>%
+  summarise(Wins = n())
+
+wins_boxplot_PT <- ggplot(wins_PT,
+                          aes(y = Wins)) +
+  geom_boxplot(fill = "pink") +
+  theme_bw()
+
+grid.arrange(wins_per_team,
+             wins_per_PT,
+             wins_boxplot,
+             wins_boxplot_PT,
+             ncol = 2)
+
 
 # Find top riders
 top_10_riders <- worldtour_2024 %>%
@@ -49,8 +73,10 @@ top_10_riders <- worldtour_2024 %>%
   slice_max(n = 11, order_by = Wins)
 print(top_10_riders)
 
-# Timeseries of "Big 4"
-big_four_TS <- ggplot(worldtour_2024,
-                      aes(x = Date,
-                          y = )
+
+
+
+
+
+
 
